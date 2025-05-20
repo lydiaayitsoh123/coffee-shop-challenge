a6328e6df55948efc33c0c4a07a1efbb29d17afe
LYDIA <lydiaayitsoh@gmail.com>
2025-05-20 11:43:43 +0300

    made changes

---
+++ customer.py
@@ -0,0 +1,33 @@
+from order import Order
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters.")
+
+    def orders(self):
+        return [order for order in Order.all_orders if order.customer == self]
+
+    def coffees(self):
+        return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee, price):
+        return Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        spending = {}
+        for order in Order.all_orders:
+            if order.coffee == coffee:
+                spending[order.customer] = spending.get(order.customer, 0) + order.price
+        return max(spending, key=spending.get, default=None)
---
+++ order.py
@@ -0,0 +1,22 @@
+class Order:
+    all_orders = []
+
+    def __init__(self, customer, coffee, price):
+        if not isinstance(price, float) or not (1.0 <= price <= 10.0):
+            raise ValueError("Price must be a float between 1.0 and 10.0")
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price
+        Order.all_orders.append(self)
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @property
+    def price(self):
+        return self._price
